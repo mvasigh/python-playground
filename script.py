1e030bb31a81bdb529b600a2fcd44e7da808599b
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-04-25 23:11:08 -0500

    add basic functions

--- script.py
+++ script.py
@@ -1,7 +1,31 @@
-for n in range(2, 10):
-  for x in range(2, n):
-    if n % x == 0:
-      print(n, 'equals', x, '*', n//x)
-      break
-  else:
-    print(n, 'is a prime number') # for loops have else blocks!
+def fib_print(n):
+  """Print a Fibonacci series up to n"""
+  a, b = 0, 1
+  while a < n:
+    print(a, end=' ')
+    a, b = b, a + b
+  print()
+
+def fib(n):
+  """Returns a list of Fibonacci numbers up to n"""
+  a, b = 0, 1
+  results = []
+  while a < n:
+    results.append(a)
+    a, b = b, a + b
+  return results
+
+def ask_ok(prompt, retries=4, reminder='Please try again'):
+  """Prompt user for confirmation and optimally allow retries"""
+  while True:
+    ok = input(prompt)
+    if ok in ('y', 'ye', 'yes'):
+      return True
+    if ok in ('n', 'no', 'nop', 'nope'):
+      return False
+    retries = retries - 1
+    if retries < 0:
+      raise ValueError('Invalid user response')
+    print(reminder)
+
+ask_ok('Do you really want to quit?', 6)
