69972a9bab2ac26891a09b775532eebb991444c7
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-05-02 20:59:46 -0500

    tuples, packing and unpacking

--- data_structures.py
+++ data_structures.py
@@ -1,4 +1,6 @@
 from collections import deque
+from math import pi
+
 queue = deque(['Mehdi', 'Mel', 'Kelvin', 'Jeff'])
 queue.append('Mooji')
 print(queue)
@@ -11,3 +13,29 @@ for x in range(10):
   squares.append(x**2)
 
 print(squares)
+
+squares_map = list(map(lambda x: x**2, range(10)))
+print('Using the map and lamba method', squares_map)
+
+squares_short = [x**2 for x in range(10)]
+print('Using the shortcut', squares_short)
+
+# sweet list comprehension example
+combined_list = [(x, y) for x in [1, 2, 3] for y in [3, 1, 4] if x != y]
+print(combined_list)
+
+print([str(round(pi, i)) for i in range(1, 6)])
+
+# tuple packing
+t = 'hello', 'world', 'universe'
+print(t)
+
+# tuple unpacking
+msg1, msg2, msg3 = t
+print(msg1, msg2, msg3)
+
+# multiple variable assignment is just a combintation of 
+# tuple packing and sequence unpacking
+# unpacking works with lists too, like destructuring in JS
+man, cat = ['Mehdi', 'Mooji']
+print(man, cat)
